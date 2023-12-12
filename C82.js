9de7fb234d05c2698d7cb76e58281ea24a8c80df
Muichirokun <149724834+Muichirokun@users.noreply.github.com>
2023-12-12 09:50:42 -0300

    Add files via upload

---
+++ C82.js
@@ -0,0 +1,29 @@
+console.log("Meu primeiro projeto no console");
+
+            var firstName = "Rebeca";
+            var lastName = "Kapoor";
+
+
+            var fullName = firstName.concat(lastName);
+            console.log(fullName);
+
+            var a = 10;
+            var b = 5;
+
+var add = a + b
+
+            var add = console.log(add);
+            console.log( );
+
+var sub = a - b
+
+      var sub = console.log(sub);
+      console.log( );
+
+var multiple = a * b
+
+            var multiple = console.log(multiple);
+            console.log();
+
+            var divide = a / b ;
+            console.log(divide);
